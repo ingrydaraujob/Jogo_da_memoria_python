8014b08e21b17c79679b8b186e3c582eaea535eb
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-10-24 01:02:39 -0300

    Update jogo da memoria.py

--- jogo da memoria.py
+++ jogo da memoria.py
@@ -7,7 +7,7 @@ num_linha = 4
 num_colunas = 4
 cartao_size_w = 10
 cartao_size_h = 5
-cores_cartao = ["red", "blue", "green", "yellow", "purple", "orage", "cyan", "magenta"]
+cores_cartao = ["red", "blue", "green", "yellow", "purple", "orange", "cyan", "magenta"]
 cor_fundo = "#343a40"
 cor_letra = "#ffffff"
 font_style = ("arial",12,"bold")
@@ -35,19 +35,19 @@ def card_clicked(linha, coluna):
         cartao["bg"] = grid[linha][coluna]
         cartao_revelado.append(cartao)
         if len(cartao_revelado) == 2:
-            check_math()
+            check_match()
 
 #verificar se os dois cartoes revelados sao iguais 
 def check_match():
-    cartao1 , cartao2 = cartao_revelado
-    if cartao1["bg"] == cartao2["bg"]:
-        cartao1.after(1000, cartao1.destroy)
-        cartao2.after(1000, cartao2.destroy)
-        cartas_correspondentes.extend([cartao1, cartao2])
+    carta1 , carta2 = cartao_revelado
+    if carta1["bg"] == carta2["bg"]:
+        carta1.after(1000, carta1.destroy)
+        carta2.after(1000, carta2.destroy)
+        cartas_correspondentes.extend([carta1, carta2])
         check_win()
     else:
-        cartao1.after(1000, lambda:cartao1.config(bg="black"))
-        cartao2.after(1000, lambda:cartao2.config(bg="black"))
+        carta1.after(1000, lambda:carta1.config(bg="black"))
+        carta2.after(1000, lambda:carta2.config(bg="black"))
     cartao_revelado.clear()
     update_score()
 
@@ -55,15 +55,18 @@ def check_match():
 
 #verificar se o jogador ganhou 
 def check_win():
-    if len(cartas_correspondentes) == num_linha == num_colunas:
-        messagebox.showinfo("paraben! ", "voce ganhou o jogo!!")
+    if len(cartas_correspondentes) == num_linha * num_colunas:
+        messagebox.showinfo("parabens! ", "voce ganhou o jogo!!")
         janela.quit()
 
 #atualizar a potuação e verificar se o jogador perdeu p jogo 
 def update_score():
-    global tentativas
-    tentativas += 1
-    label_tentativas.config(text="Tentativas: {}/{} ".format(tentativas, max_tentativas))
+    global numero_tentativas
+    numero_tentativas += 1
+    label_tentativas.config(text="Tentativas: {}/{} ".format(numero_tentativas, max_tentativas))
+    if numero_tentativas >= max_tentativas:
+        messagebox.showinfo("fim de jogo", "voce perdeu o jogo")
+        janela.quit()
 
 
 #interface principal
@@ -83,7 +86,7 @@ numero_tentativas = 0
 for linha in range (num_linha):
     linha_de_cartoes = []
     for col in range(num_colunas):
-        cartao = tk.Button(janela, width=cartao_size_w, height=cartao_size_h, bg="black", relief= tk.RAISED, bd=3)
+        cartao = tk.Button(janela, width=cartao_size_w, height=cartao_size_h, bg="black", relief= tk.RAISED, bd=3, command=lambda r=linha, c=col: card_clicked(r,c))
         cartao.grid(row=linha, column= col, padx= 5 , pady= 5 )
         linha_de_cartoes.append(cartao)
     cartoes.append(linha_de_cartoes)
@@ -97,7 +100,7 @@ button_style = {"activebackground ": "#f8f9fa" , "font":font_style, "fg":cor_let
 janela.option_add("Button", button_style) 
 
 #numeros de tentativas
-label_tentativas = tk.Label(janela, text="Tentativas:{}/{}".format(tentativas, max_tentativas),fg=cor_letra, bg = cor_fundo , font=font_style)
+label_tentativas = tk.Label(janela, text="Tentativas:{}/{}".format(numero_tentativas, max_tentativas),fg=cor_letra, bg = cor_fundo , font=font_style)
 label_tentativas.grid(row=num_linha , column= num_colunas, padx=10 , pady=10 )
 
 
